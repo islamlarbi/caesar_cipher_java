c8f4c5600fbff789fbb96db8f68fb32cda40a21b
islamdev_7 <larbi19960123@gmail.com>
2019-04-27 18:57:15 +0100

    more simple to use

--- src/caesar_cipher_java/Caesar_cipher_java.java
+++ src/caesar_cipher_java/Caesar_cipher_java.java
@@ -2,6 +2,7 @@
  * free without licence
  * free without licence
  * free without licence
+ *  code club djelfa fb/CodeClubDjelfa
  */
 package caesar_cipher_java;
 
@@ -9,13 +10,13 @@ import java.util.Scanner;
 
 /**
  *
- * @author islamlarbi
+ * @author islamlarbi / code club djelfa
  */
 public class Caesar_cipher_java {
 
 public static void main(String[] args){// main function 
                 String text="y jhyut huqsxydw ekj je jxu secfqdo qdt aufj husuylydw qkjecqjut huifediui";//Example of encrypted text you can change text here or use scanner 
-                String []word_expected={"to","ahmed"};//word_expected You can add any number of texts The important thing in it is at least one valid and justified in the encoded text
+                String []word_expected={"to","ahmed"};//word_expected You can add any number of texts , The important thing in it is at least one valid and justified in the encoded text
 		System.out.println("textEncrypted : \n"+Decryption_cezar(word_expected,text));// result use one function only 
 
 }// end main function 
@@ -24,28 +25,30 @@ public static String Decryption_cezar(String []word_expected,String textEncrypte
 		String text="";// text clair
 		textEncrypted=textEncrypted.toLowerCase(); // characters to lower characters
                 
-                   int key_encryption=1; // key encryption we start with value equal 1 
+                   int key_encryption=1; // key encryption we start with value equal 1
                   
-                for(int i=0;i<textEncrypted.length();i++) {// text من اجل استخراج احرف
-			for(int j=0;j<26;j++) {// alphabet من اجل استخراج احرف
-                            if(textEncrypted.charAt(i)==alphabet.charAt(j))//المقارنة بين الاحرف 
-                             text=text+alphabet.charAt((j+key_encryption)%26);// التشفير بالحرف 
-                            if(textEncrypted.charAt(i)==' '){text=text+' ';break; 
-			}
+                for(int i=0;i<textEncrypted.length();i++) {//An external loop for extracting characters in encrypted text
+			for(int j=0;j<26;j++) {// An internal loop to shift the character according to the encryption key
+                            if(textEncrypted.charAt(i)==alphabet.charAt(j))//Comparing encrypted text characters and alphabet characters 
+                             text=text+alphabet.charAt((j+key_encryption)%26);// Decrypt the character
+                            if(textEncrypted.charAt(i)==' '){text=text+' ';break;}// End of the word , The word ends when there is a space
 		}
-           if((testin(word_expected,text)&&text.length()==textEncrypted.length()-1)||key_encryption>26){break;}
-           else{if(i==textEncrypted.length()-1){key_encryption++;text=""; i=0;}}
+                      //  The real secret is all here, focus here , you see this condition is located at first loop 
+                      // you can see the function testin before this condition 
+           if((testin(word_expected,text)&&text.length()==textEncrypted.length()-1)||key_encryption>26){break;} 
+           if(i==textEncrypted.length()-1){key_encryption++;text=""; i=0;}
 
               }  
 
  return text;
 }
-public static boolean testin(String[] a,String b){
- String f[] = b.split(" ");
- for(int i=0;i<f.length;i++){
-    for(int k=0;k<a.length;k++) { 
-           if(f[i].equals(a[k])){  return true;}}
-                             
- } return false;
+public static boolean testin(String[] expected_word,String text_clair){// we search if one of expected word existe in text after decryption
+ String text_clair_array[] = text_clair.split(" ");// Convert text to array for In order to compare characters 
+ for(int i=0;i<text_clair_array.length;i++){
+    for(int k=0;k<expected_word.length;k++) { 
+           if(text_clair_array[i].equals(expected_word[k])){  return true;}}// if one of expected word existe in text after decryption return true                            
+         } 
+        return false;// return false if no one of expected word existe in text for more test authre jey 
 }
 }
+
