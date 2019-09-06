80d58b02fd6230580a7cbb6e78950f50ce2169f2
islamdev_7 <larbi19960123@gmail.com>
2019-09-06 16:53:43 -0700

    Update Caesar_cipher_java.java

--- src/caesar_cipher_java/Caesar_cipher_java.java
+++ src/caesar_cipher_java/Caesar_cipher_java.java
@@ -8,9 +8,6 @@ package caesar_cipher_java;
 
 import java.util.Scanner;
 
-/**
- * @author islamlarbi / code club djelfa
- */
 public class Caesar_cipher_java {
 
 public static void main(String[] args){// main function 
